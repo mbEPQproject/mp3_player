112d5cac73393f40edfd59538073227424635ca8
mbEPQproject <mb164467@hrsfc.ac.uk>
2025-03-09 22:46:34 +0000

    added audioplayers -> made a AudioTest function and added a test audio (if i had a gun - buckshot)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(AudioTest());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +10,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class AudioTest extends StatelessWidget {
+  const AudioTest({super.key});
+
+  void play() async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    await audioPlayer.play(AssetSource('test_audio.mp3'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Audio Test')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: play,
+            child: Text('Play Audio'),
+          ),
+        ),
+      ),
+    );
   }
 }
