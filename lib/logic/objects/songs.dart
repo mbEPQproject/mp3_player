360f4d062e47b6cf15d1c40da9107bcf38c4adec
mbEPQproject <mb164467@hrsfc.ac.uk>
2025-06-18 18:22:58 +0100

    added a very basic constructor to song class

--- lib/logic/objects/songs.dart
+++ lib/logic/objects/songs.dart
@@ -4,7 +4,6 @@ class Song {
   late String songUrl;
   late String title;
   late Object trackNumber;
-}
 
-//TODO: initialiser which takes the metadata from the audio file
-    // -> this should occur when the app launches, perhaps in a 'loading' screen prior to the first screen being displayed
+  Song(this.songUrl, this.title, this.trackNumber);
+}
