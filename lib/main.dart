4977a20e3b8fb06c4ce3bca54870f1e96ab02e80
mbEPQproject <mb164467@hrsfc.ac.uk>
2025-07-30 14:17:39 +0100

    removed debug banner + testLoadSongs()
    testLoadSongs no longer needed as I learnt how to go from opening_loadingscreen meaning all the songs get loaded in

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,11 @@ import 'ui/song_select.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // testLoadSongs();
-
-  //print(Globals.albums[0].songs[0].title);
-
   runApp(
     ChangeNotifierProvider(
       create: (context) => AudioPlayerLogic(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: const OpeningLoadingScreen(),
         routes: {
           '/album_select': (context) => AlbumSelect(),
@@ -37,51 +34,4 @@ void main() async {
       ),
     ),
   );
-}
-
-// allows for lloading songs without needing to use the opening_loadingscreen as i dont know how to swtich from it
-void testLoadSongs() {
-  Directory mainDir = Directory('/home/ohbowie/Downloads/music/');
-  List<FileSystemEntity> artists = mainDir.listSync();
-
-  for (FileSystemEntity artist in artists) {
-    if (artist is File) continue;
-    Directory artistDir = Directory(artist.path);
-    List<FileSystemEntity> albums = artistDir.listSync();
-
-    for (FileSystemEntity album in albums) {
-      Directory albumDir = Directory(album.path);
-      List<FileSystemEntity> songs = albumDir.listSync();
-
-      Album newAlbum = Album();
-      newAlbum.albumArtPath = '';
-
-      newAlbum.title = albumDir.path.substring(artist.path.length + 1);
-      newAlbum.artist = artistDir.path.substring(mainDir.path.length);
-
-      int count = 0;
-      for (FileSystemEntity file in songs) {
-        if (file.path.endsWith('.jpg') ||
-            file.path.endsWith('.png') ||
-            file.path.endsWith('.jpeg')) {
-          newAlbum.albumArtPath = file.path;
-          continue;
-        }
-        Song newSong = Song();
-        newSong.songPath = file.path;
-        newSong.title = file.path.substring(albumDir.path.length + 1);
-        newSong.trackNumber = count + 1;
-
-        newAlbum.songs.add(newSong);
-
-        count++;
-      }
-      newAlbum.amountOfSongs = count;
-
-      if (newAlbum.albumArtPath == '') {
-        newAlbum.albumArtPath = defaultAlbumArtPath;
-      }
-      Globals.albums.add(newAlbum);
-    }
-  }
 }
