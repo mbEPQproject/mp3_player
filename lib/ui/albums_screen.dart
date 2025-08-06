ebb745ac5234230182785e4165ec4366c9544e1b
mbEPQproject <mb164467@hrsfc.ac.uk>
2025-08-06 18:04:26 +0100

    added functioning appBar into albums screen

--- lib/ui/albums_screen.dart
+++ lib/ui/albums_screen.dart
@@ -12,6 +12,58 @@ class AlbumsScreen extends StatefulWidget {
 class _AlbumsScreenState extends State<AlbumsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(
+          const Color.fromARGB(255, 236, 236, 236).toARGB32(),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/home_screen');
+                },
+                style: ButtonStyle(
+                  overlayColor: WidgetStateProperty.all(Colors.transparent),
+                ),
+                child: Text('Home', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/queue_screen');
+                },
+                style: ButtonStyle(
+                  overlayColor: WidgetStateProperty.all(Colors.transparent),
+                ),
+                child: Text('Queue', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  overlayColor: WidgetStateProperty.all(Colors.transparent),
+                  backgroundColor: WidgetStateProperty.all(
+                    Color.fromARGB(255, 221, 221, 221),
+                  ),
+                ),
+                child: Text('Albums', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Placeholder(),
+    );
   }
 }
